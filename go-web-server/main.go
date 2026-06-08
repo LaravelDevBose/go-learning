54b95de6c5df77f0057ce4d14b48b8387d9cbdd0
Arup Bose <laravel.devbose@gmail.com>
2026-06-08 23:20:54 +0600

    init basic http server and assets file load

--- go-web-server/main.go
+++ go-web-server/main.go
@@ -1,1 +1,20 @@
-package gowebserver
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	fmt.Println("Starting server at port 80")
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Go Lovvers!")
+}
