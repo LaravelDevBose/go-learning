cea9b987673263f0967e7a33c5a16a6d82f41aff
Arup Bose <laravel.devbose@gmail.com>
2026-06-08 22:57:24 +0600

    booking app added

---
+++ booking-app/main.go
@@ -0,0 +1,112 @@
+// booking-app/main.go
+package main
+
+import (
+	"fmt"
+)
+
+const conferenceTickets int = 50
+
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+var bookings = make([]BookingData, 0)
+
+type BookingData struct {
+	firstName    string
+	lastName     string
+	email        string
+	numOfTickets uint
+}
+
+var GlobalVaiable string = "When variable start with Capital latter then its become global vaiable can access from anyware of app"
+
+func main() {
+
+	greetUser()
+
+	for {
+
+		firstName, lastName, email, userTickets := getUserInputs()
+
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInputs(firstName, lastName, email, userTickets, remainingTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			bookTicket(firstName, lastName, email, userTickets)
+
+			firstnames := getFirstNames()
+			fmt.Printf("The first names of bookings are : %v \n", firstnames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you enterd is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered dons't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("your number of ticket is invalid")
+			}
+		}
+	}
+
+}
+
+func greetUser() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("we have total of %v tickets and %v are available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames() []string {
+	firstnames := []string{}
+	for _, booking := range bookings {
+		firstnames = append(firstnames, booking.firstName)
+	}
+	return firstnames
+}
+
+func getUserInputs() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+
+	fmt.Print("Enter Your First Name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+
+}
+
+func bookTicket(firstName string, lastName string, email string, userTickets uint) {
+	userName := firstName + " " + lastName
+	remainingTickets = remainingTickets - userTickets
+
+	var bookingData = BookingData{
+		firstName:    firstName,
+		lastName:     lastName,
+		email:        email,
+		numOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, bookingData)
+
+	fmt.Printf("Thank You %v for booking %v tickets. You will receive a confermation email at %v \n", userName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
