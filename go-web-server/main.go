eb0bcf73ab09e18b4f21a4f097fb215648bf73e4
Arup Bose <laravel.devbose@gmail.com>
2026-06-08 23:56:21 +0600

    post request get data

--- go-web-server/main.go
+++ go-web-server/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type PostBody struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/post", postRequestDetails)
 
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
@@ -21,4 +28,21 @@ func index(w http.ResponseWriter, r *http.Request) {
 	println("Request method:", r.Method)
 	params := fmt.Sprintf("Request Params: %s\n", r.URL.Query().Get("name"))
 	fmt.Fprintf(w, "%s", params)
+}
+
+func postRequestDetails(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Fprintf(w, "Only POST method is allowed")
+		return
+	}
+	defer r.Body.Close()
+	var postBody PostBody
+	err := json.NewDecoder(r.Body).Decode(&postBody)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Received POST request with body: %+v \n", postBody)
+	fmt.Fprintf(w, "Name: %+v\n", postBody.Name)
+	fmt.Fprintf(w, "Email: %+v\n", postBody.Email)
 }
