ec18924c213443a23e430e230a0228d9cc8f1d8c
Arup Bose <laravel.devbose@gmail.com>
2026-06-12 19:46:03 +0600

    book create, update and delete done

--- bookstore/pkg/controllers/bookController.go
+++ bookstore/pkg/controllers/bookController.go
@@ -53,8 +53,74 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
-func CreateBook(w http.ResponseWriter, r *http.Request) {}
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
 
-func UpdateBook(w http.ResponseWriter, r *http.Request) {}
+	newBook := CreateBook.CreateBook()
 
-func DeleteBook(w http.ResponseWriter, r *http.Request) {}
+	response, err := (&utils.Response{}).PrepareResponse(newBook, http.StatusCreated, "Book Created Successfully")
+	if err != nil {
+		http.Error(w, "Failed to prepare response", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+
+	bookId := mux.Vars(r)["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		response, _ := (&utils.Response{}).PrepareResponse(nil, http.StatusBadRequest, "Invalid book ID")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+		return
+	}
+
+	bookDetail, _ := models.GetBookById(ID)
+
+	if bookDetail.ID == 0 {
+		response, _ := (&utils.Response{}).PrepareResponse(nil, http.StatusNotFound, "Book not found")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(response)
+		return
+	}
+
+	updatedBook := updateBook.UpdateBook(ID)
+	response, err := (&utils.Response{}).PrepareResponse(updatedBook, http.StatusOK, "Book updated successfully")
+	if err != nil {
+		http.Error(w, "Failed to prepare response", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	bookId := mux.Vars(r)["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		response, _ := (&utils.Response{}).PrepareResponse(nil, http.StatusBadRequest, "Invalid book ID")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+		return
+	}
+
+	book := models.DeleteBookById(ID)
+
+	response, err := (&utils.Response{}).PrepareResponse(book, http.StatusOK, "Book deleted successfully")
+	if err != nil {
+		http.Error(w, "Failed to prepare response", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
+}
